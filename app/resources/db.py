66cf0af54b864013ceecd0f1bd444f6c29320bd3
kjacmenja21 <karlojacmenjak@gmail.com>
2024-11-15 18:48:17 +0100

    Apply settings to engine

--- app/resources/db.py
+++ app/resources/db.py
@@ -3,12 +3,14 @@ import typing
 
 from sqlalchemy.ext import asyncio as sa
 
+from app.settings import Settings
+
 logger = logging.getLogger(__name__)
 
 
-async def create_sa_engine():
+async def create_sa_engine() -> typing.AsyncIterator[sa.AsyncEngine]:
     logger.info("Initializing SQLAlchemy engine")
-    engine = sa.create_async_engine()
+    engine = sa.create_async_engine(str(Settings()))
     logger.info("SQLAlchemy engine has been initialized")
 
     try:
