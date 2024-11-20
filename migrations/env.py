ff3006777b900ee9ef2b1d9f58412306ed2a90a7
kjacmenja21 <karlojacmenjak@gmail.com>
2024-11-20 10:35:18 +0100

    Insert sqlalchemy_url using env.py

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,9 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
+
+from app.database.settings import db_settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -13,6 +13,10 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+    print(db_settings.db_dsn)
+    config.set_main_option(
+        "sqlalchemy.url", db_settings.db_dsn.render_as_string(hide_password=False)
+    )
 
 # add your model's MetaData object here
 # for 'autogenerate' support
@@ -64,9 +68,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
