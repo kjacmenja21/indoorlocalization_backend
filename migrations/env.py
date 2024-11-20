8cff91b621a160213e05464758c64bf303208e49
kjacmenja21 <karlojacmenjak@gmail.com>
2024-11-20 10:59:59 +0100

    Set target_metadata to Base.metadata in env.py

--- migrations/env.py
+++ migrations/env.py
@@ -4,6 +4,7 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 
 from app.database.settings import db_settings
+from app.models.common import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
