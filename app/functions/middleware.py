c105a76fea028eeb23f8022a81957c17aef55a5d
kjacmenja21 <karlojacmenjak@gmail.com>
2024-11-20 10:59:42 +0100

    Call init_orm in lifespan

--- app/functions/middleware.py
+++ app/functions/middleware.py
@@ -3,9 +3,12 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from app.models.common import init_orm
+
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
-    logging.warning(f"Starting the application: ENV= # TODO ADD ENV LOADING")
+    logging.info(f"Starting the application: ENV= # TODO ADD ENV LOADING")
+    init_orm()
     yield
     logging.warning("Shutting down the application")
