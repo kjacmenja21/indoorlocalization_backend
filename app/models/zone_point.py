8b6497bbfe5f9b7208c2d7bbb78c8cc6a8fbcdae
kjacmenja21 <karlojacmenjak@gmail.com>
2024-12-02 18:31:46 +0100

    Add Zone model

--- app/models/zone_point.py
+++ app/models/zone_point.py
@@ -1,10 +1,18 @@
-from sqlalchemy import Float, Integer
+from sqlalchemy import Float, ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.models.common import Base
 
 
+class Zone(Base):
+    __tablename__ = "zone"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(80))
+    floorMapId: Mapped[int] = mapped_column(ForeignKey("floorMap.id"))
+
+
 class ZonePoint(Base):
+    __tablename__ = "zonePoint"
     zoneId: Mapped[int] = mapped_column(Integer, primary_key=True)
     ordinalNumber: Mapped[int] = mapped_column(Integer)
     x: Mapped[Float] = mapped_column(Float)
