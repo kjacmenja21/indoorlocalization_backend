6a0f6c378b8609263a354f219d025aeef4799b8c
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-03 11:31:28 +0100

    Refactor function to class to fix zeroconf._exceptions.EventLoopBlocked

--- app/functions/multicast_dns.py
+++ app/functions/multicast_dns.py
@@ -1,17 +1,29 @@
+import asyncio
+import logging
+
 from zeroconf import ServiceInfo, Zeroconf
 
 
-def register_service(hostname: str, port: int) -> tuple[Zeroconf, ServiceInfo]:
-    zeroconf = Zeroconf()
-    service_type = "_http._tcp.local."
-    service_name = f"{hostname}.{service_type}"
-    service_info = ServiceInfo(
-        type_=service_type,
-        name=service_name,
-        address=b"\x00\x00\x00\x00",  # Placeholder; this will be auto-detected
-        port=port,
-        properties={},
-        server=f"{hostname}.local.",
-    )
-    zeroconf.register_service(service_info)
-    return zeroconf, service_info
+class MulticastDNS:
+    def __init__(self, hostname: str, port: int) -> None:
+        self.zeroconf = Zeroconf()
+        self.service_type = "_http._tcp.local."
+        self.hostname = hostname
+        self.port = port
+        self.service_info = ServiceInfo(
+            type_=self.service_type,
+            name=f"{self.hostname}.{self.service_type}",
+            addresses=[b"\x00\x00\x00\x00"],  # Placeholder; this will be auto-detected
+            port=self.port,
+            properties={},
+            server=f"{self.hostname}.local.",
+        )
+
+    async def register_service(self):
+        logging.info(f"Registering service {self.service_info.name}")
+        await asyncio.to_thread(self.zeroconf.register_service, self.service_info)
+
+    async def unregister_service(self):
+        logging.info(f"Unregistering service {self.service_info.name}")
+        await asyncio.to_thread(self.zeroconf.unregister_service, self.service_info)
+        await asyncio.to_thread(self.zeroconf.close)
