a031d649b7df8c82dc8d93932502b3b80e50064e
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-03 19:15:56 +0100

    Add config option to toggle  multicastDNS

--- app/functions/middleware.py
+++ app/functions/middleware.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from app.config import GeneralConfig
 from app.functions.logger import setup_logger
 from app.functions.multicast_dns import MulticastDNS
 from app.models.common import init_orm
@@ -11,10 +12,18 @@ from app.models.common import init_orm
 @asynccontextmanager
 async def lifespan(_: FastAPI):
     setup_logger()
-    multicast_dns = MulticastDNS("indoor_localization", 8001)
     logging.info("Starting the application")
+
+    config = GeneralConfig()
+    multicast_dns: MulticastDNS | None = None
+
     init_orm()
-    await multicast_dns.register_service()
+
+    if config.use_multicast_dns:
+        multicast_dns = MulticastDNS("indoor_localization", 8001)
+        await multicast_dns.register_service()
     yield
-    await multicast_dns.unregister_service()
+
+    if multicast_dns:
+        await multicast_dns.unregister_service()
     logging.warning("Shutting down the application")
