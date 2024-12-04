f78397e3d13adfef771581a62d50deb63704a4d2
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-04 19:55:45 +0100

    Import repositories in __init__.py for easier importing

---
+++ app/database/repositories/__init__.py
@@ -0,0 +1,5 @@
+from app.database.repositories.irepository import IRepository
+from app.database.repositories.sqlalchemy_repository import SQLAlchemyRepository
+from app.database.repositories.user_repository import UserRepository
+
+__all__ = ["SQLAlchemyRepository", "IRepository", "UserRepository"]
