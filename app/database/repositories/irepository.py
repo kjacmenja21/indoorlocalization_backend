1cc191cd2b337f28fe8603f1098930a353034816
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-04 19:08:29 +0100

    Add IRepository abstract class

---
+++ app/database/repositories/irepository.py
@@ -0,0 +1,20 @@
+from typing import Generic, List, Optional, TypeVar
+
+T = TypeVar("T")
+
+
+class IRepository(Generic[T]):
+    def add(self, entity: T) -> None:
+        raise NotImplementedError
+
+    def get(self, id: int) -> Optional[T]:
+        raise NotImplementedError
+
+    def get_all(self) -> List[T]:
+        raise NotImplementedError
+
+    def update(self, entity: T) -> None:
+        raise NotImplementedError
+
+    def delete(self, id: int) -> None:
+        raise NotImplementedError
