a402eefdb31e37299996bb0b47aca09cd73a535b
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-04 18:40:09 +0100

    Add auth router to v1 API

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from app.api.v1.auth import auth_router
 from app.api.v1.test_file import test_router
 
 v1_router = APIRouter(prefix="/v1")
 
 v1_router.include_router(test_router)
+v1_router.include_router(auth_router)
