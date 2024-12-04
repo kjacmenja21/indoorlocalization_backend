fe79d73eaa38b4d31d32bebbbb39a2cec1beeaad
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-04 22:16:40 +0100

    Fix missing typing in jwt.py

--- app/functions/jwt.py
+++ app/functions/jwt.py
@@ -21,7 +21,7 @@ def verify_password(plain_password, hashed_password) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def get_password_hash(password) -> str:
+def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
 
