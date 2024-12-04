45eff90d95b6110af217c4cd65066de37894070d
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-04 22:35:29 +0100

    Fix get_user_service not working

--- app/api/v1/dependancies.py
+++ app/api/v1/dependancies.py
@@ -1,15 +1,14 @@
 from typing import Annotated
 
 from fastapi import Depends
-from sqlalchemy.orm import Session
 
 from app.database.db import get_db_session
 from app.database.repositories import UserRepository
 from app.database.services import UserService
 
 
-def get_user_service() -> UserService:
-    return UserService(UserRepository(get_db_session()))
+def get_user_service(session=Depends(get_db_session)) -> UserService:
+    return UserService(UserRepository(session=session))
 
 
 UserServiceDep = Annotated[UserService, Depends(get_user_service)]
