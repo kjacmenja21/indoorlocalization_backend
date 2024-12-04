af2b01f9ed840e3f762d0cbe539d9b075fcd8d5c
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-04 17:27:11 +0100

    Add minimal auth code without business logic

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,3 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app.schemas.auth.token import Token
 
 auth_router = APIRouter(prefix="/auth", tags=["Auth"])
+
+
+@auth_router.get("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends()) -> Token:
+    user = None  # authenticate_user
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = None  # create_access_token
+    return Token()
