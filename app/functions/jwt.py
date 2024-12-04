62c621b4f3c94c6dde1e3c139b73309207fc3836
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-04 16:36:56 +0100

    Add functions to create access_token

--- app/functions/jwt.py
+++ app/functions/jwt.py
@@ -1,10 +1,15 @@
-from datetime import datetime, timedelta
+import datetime
+from datetime import datetime as dt
+from datetime import timedelta
 
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+from pydantic import BaseModel
+
+from app.config import JWTConfig
 
 # Secret key to encode and decode JWT
-
+config = JWTConfig()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -15,3 +20,24 @@ def verify_password(plain_password, hashed_password) -> bool:
 
 def get_password_hash(password) -> str:
     return pwd_context.hash(password)
+
+
+def create_access_token(data: BaseModel, expires_delta: timedelta | None = None) -> str:
+
+    to_encode = data.model_dump()
+
+    if expires_delta:
+        expire = dt.now(datetime.timezone.utc) + expires_delta
+    else:
+        expire = dt.now(datetime.timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, config.secret_key, algorithm=config.algorithm)
+    return encoded_jwt
+
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, config.secret_key, algorithms=[config.algorithm])
+        return payload
+    except JWTError:
+        return None
