1884d002182cf3d17b25e9fec3d4ee1625d86c6b
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-04 17:40:53 +0100

    Add decode method to Token schema

--- app/schemas/auth/token.py
+++ app/schemas/auth/token.py
@@ -1,6 +1,21 @@
 from pydantic import BaseModel
 
+from app.functions.exceptions import forbidden
+from app.functions.jwt import decode_access_token
+from app.schemas.auth.token_extra import TokenDecode
+from app.schemas.auth.user import Role
+
 
 class Token(BaseModel):
     access_token: str
-    token_type: str
+    token_type: str = "Bearer"
+
+    @classmethod
+    def decode(cls, token: str, scope: list[Role] | None = None) -> TokenDecode:
+        decoded = decode_access_token(token)
+        if scope:
+            if "admin" in decoded["scope"]:
+                ...
+            elif not all(i in decoded["scope"] for i in scope):
+                raise forbidden(msg="Insufficient scope.")
+        return TokenDecode.model_validate(decoded)
