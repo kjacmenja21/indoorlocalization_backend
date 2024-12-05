de325d8b210691cea6b73b919ccf249a880a9c9b
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-05 20:15:33 +0100

    Remove IUserRepository from UserService

--- app/database/services/user_service.py
+++ app/database/services/user_service.py
@@ -1,24 +1,29 @@
-from app.database.repositories.interfaces.iuserrepository import IUserRepository
+from sqlalchemy.orm import Session
+
+from app.database.db import get_db_session
 from app.functions.jwt import verify_password
 from app.models.user import User
 from app.schemas.api.user import UserCreate, UserModel
 
 
 class UserService:
-    def __init__(self, repository: IUserRepository) -> None:
-        self.repository: IUserRepository = repository
+    def __init__(self, session: Session) -> None:
+        self.session = session
 
     def authenticate_user(self, username: str, password: str) -> None | User:
-        user = self.repository.get_user_by_username(username)
+        user = self.session.query(User).where(User.username == username).first()
 
         if user is None:
             return None
 
-        if verify_password(password, user.salt):
-            return self.repository.get(user.id)
+        if verify_password(password, user.password):
+            return user
 
     def create_user(self, user: UserCreate) -> User:
-        user = UserModel(**user.model_dump())
-        created_user = self.repository.add(user)
+        create_dump = user.model_dump()
+        create_dump.update()
+
+        db_user = UserModel()
+        created_user = self.repository.add(db_user)
 
         return created_user
