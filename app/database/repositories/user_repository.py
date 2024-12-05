e993d95e2ac1a2eb22af0b10d734075ce2cf54c2
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-05 13:40:45 +0100

    Implement IUserRepository instead of SQLAlchemyRepository

--- app/database/repositories/user_repository.py
+++ app/database/repositories/user_repository.py
@@ -1,6 +1,8 @@
-from app.database.repositories import SQLAlchemyRepository
+from app.database.repositories.interfaces.iuserrepository import IUserRepository
 from app.models.user import User
-from app.schemas.db.user import UserModel
 
 
-class UserRepository(SQLAlchemyRepository[User, UserModel]): ...
+class UserRepository(IUserRepository):
+    def get_user_by_username(self, username: str) -> None | User:
+        user = self.db_session.query(User).where(User.username == username).first()
+        return user
