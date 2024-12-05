fbcdf74fbb0550f47d924a470f31c94eec87fb61
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-05 13:45:19 +0100

    Add typing to authenticate_user

--- app/database/services/user_service.py
+++ app/database/services/user_service.py
@@ -1,12 +1,13 @@
 from app.database.repositories.interfaces.iuserrepository import IUserRepository
 from app.functions.jwt import verify_password
+from app.models.user import User
 
 
 class UserService:
     def __init__(self, repository: IUserRepository) -> None:
         self.repository: IUserRepository = repository
 
-    def authenticate_user(self, username: str, password: str):
+    def authenticate_user(self, username: str, password: str) -> None | User:
         user = self.repository.get_user_by_username(username)
 
         if user is None:
