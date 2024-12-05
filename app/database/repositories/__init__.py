eb78e6a9f2b7eec86fb297d9b715595d3248ab7d
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-05 13:34:02 +0100

    Fix import for IRepository

--- app/database/repositories/__init__.py
+++ app/database/repositories/__init__.py
@@ -1,4 +1,4 @@
-from app.database.repositories.irepository import IRepository
+from app.database.repositories.interfaces.irepository import IRepository
 from app.database.repositories.sqlalchemy_repository import SQLAlchemyRepository
 from app.database.repositories.user_repository import UserRepository
 
