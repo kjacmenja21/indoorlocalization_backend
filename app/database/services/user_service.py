cccaf0a8a91c38bd82886adda3d38c28e4c94297
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-05 13:41:00 +0100

    Add authenticate_user to UserService

--- app/database/services/user_service.py
+++ app/database/services/user_service.py
@@ -1,6 +1,16 @@
-from app.database.repositories.sqlalchemy_repository import SQLAlchemyRepository
+from app.database.repositories.interfaces.iuserrepository import IUserRepository
+from app.functions.jwt import verify_password
 
 
 class UserService:
-    def __init__(self, repository: SQLAlchemyRepository) -> None:
-        self.repository = repository
+    def __init__(self, repository: IUserRepository) -> None:
+        self.repository: IUserRepository = repository
+
+    def authenticate_user(self, username: str, password: str):
+        user = self.repository.get_user_by_username(username)
+
+        if user is None:
+            return None
+
+        if verify_password(password, user.salt):
+            return self.repository.get(user.id)
