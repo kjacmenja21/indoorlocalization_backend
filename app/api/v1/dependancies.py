19afc0a220db5fa89db24794a079f0ff301bb670
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-05 14:44:36 +0100

    Remove SQLAlchemyRepository

--- app/api/v1/dependancies.py
+++ app/api/v1/dependancies.py
@@ -5,10 +5,11 @@ from fastapi import Depends
 from app.database.db import get_db_session
 from app.database.repositories import UserRepository
 from app.database.services import UserService
+from app.models.user import User
 
 
 def get_user_service(session=Depends(get_db_session)) -> UserService:
-    return UserService(UserRepository(session=session))
+    return UserService(UserRepository(model=User, session=session))
 
 
 UserServiceDep = Annotated[UserService, Depends(get_user_service)]
--- app/database/repositories/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.database.repositories.interfaces.irepository import IRepository
-from app.database.repositories.sqlalchemy_repository import SQLAlchemyRepository
-from app.database.repositories.user_repository import UserRepository
-
-__all__ = ["SQLAlchemyRepository", "IRepository", "UserRepository"]
