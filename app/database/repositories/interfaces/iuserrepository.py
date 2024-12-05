d8f75bc4b64cf628ae96de1869960be63b2807fa
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-05 20:08:41 +0100

    Remove repository pattern

--- app/database/repositories/interfaces/iuserrepository.py
+++
@@ -1,8 +0,0 @@
-from app.database.repositories.sqlalchemy_repository import SQLAlchemyRepository
-from app.models.user import User
-from app.schemas.api.user import UserModel
-
-
-class IUserRepository(SQLAlchemyRepository[User, UserModel]):
-    def get_user_by_username(self, username: str) -> None | User:
-        raise NotImplementedError
--- app/database/repositories/user_repository.py
+++
@@ -1,8 +0,0 @@
-from app.database.repositories.interfaces.iuserrepository import IUserRepository
-from app.models.user import User
-
-
-class UserRepository(IUserRepository):
-    def get_user_by_username(self, username: str) -> None | User:
-        user = self.db_session.query(User).where(User.username == username).first()
-        return user
