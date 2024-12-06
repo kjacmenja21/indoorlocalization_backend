130a2ffd10978d81f8a5f629890d0e124c76df21
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-06 19:16:46 +0100

    Add RefreshTokenData

--- app/schemas/auth/token_extra.py
+++ app/schemas/auth/token_extra.py
@@ -1,8 +1,9 @@
+import uuid
 from datetime import UTC, datetime
 from enum import Enum
 from typing import Optional
 
-from pydantic import BaseModel, ConfigDict, EmailStr, computed_field
+from pydantic import UUID4, BaseModel, ConfigDict, EmailStr, computed_field
 
 from app.schemas.auth.user import Role
 
@@ -19,6 +20,15 @@ class AccessTokenData(BaseModel):
     first_name: Optional[str] = None
     last_name: Optional[str] = None
     contract: Optional[str] = None
+
+
+class RefreshTokenData(BaseModel):
+    client_id: int
+    scope: list[Role] = [Role.USER]
+
+    @computed_field
+    def uuid(self) -> UUID4:
+        return uuid.uuid4()
 
 
 class TokenEncode(BaseModel):
