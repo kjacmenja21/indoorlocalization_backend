3f69a9ce6bfbd08fd3d52bf56d7ab7535005d0c8
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-06 21:44:40 +0100

    Use get_current_user_with_scope instead of Annotated in user_create

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -1,13 +1,9 @@
-from typing import Annotated
-
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Query
 from fastapi.responses import JSONResponse
 
 from app.api.v1.dependancies import UserServiceDep, get_current_user_with_scope
 from app.functions.exceptions import unprocessable_entity
-from app.functions.schemes import oauth2_scheme
-from app.schemas.api.user import UserCreate, UserModel
-from app.schemas.auth.token import Token
+from app.schemas.api.user import UserBase, UserCreate, UserModel
 from app.schemas.auth.user import Role
 
 user_router = APIRouter(prefix="/users", tags=["User"])
@@ -25,11 +21,10 @@ async def get_all_users(
 
 @user_router.post("/")
 def user_create(
-    user: UserCreate,
     user_service: UserServiceDep,
-    token: Annotated[str, Depends(oauth2_scheme)],
+    _: UserBase = get_current_user_with_scope([Role.ADMIN]),
+    user: UserCreate = Query(),
 ) -> JSONResponse:
-    decoded = Token.decode(token=token, scope=[Role.ADMIN])
     new_user = user_service.create_user(user)
     if not new_user:
         raise unprocessable_entity()
@@ -37,6 +32,6 @@ def user_create(
     return JSONResponse(
         {
             "message": "User successfully created.",
-            "user": new_user,
+            "user": UserModel(**new_user.model_dump()).model_dump(),
         }
     )
