699ed529345535cd932554b0e5f4fc6562023db6
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-06 14:56:35 +0100

    Add user_from_orm method to UserService

--- app/database/services/user_service.py
+++ app/database/services/user_service.py
@@ -4,21 +4,21 @@ from sqlalchemy.orm import Session, joinedload
 from app.functions.exceptions import not_found
 from app.functions.jwt import verify_password
 from app.models.user import User, UserRole
-from app.schemas.api.user import UserCreate, UserModel, UserRoleModel
+from app.schemas.api.user import UserBase, UserCreate, UserModel, UserRoleModel
 
 
 class UserService:
     def __init__(self, session: Session) -> None:
         self.session = session
 
-    def authenticate_user(self, username: str, password: str) -> None | User:
+    def authenticate_user(self, username: str, password: str) -> None | UserModel:
         user = self.session.query(User).where(User.username == username).first()
 
         if user is None:
             return None
 
         if verify_password(password, user.password):
-            return user
+            return self.user_from_orm(user)
 
     def create_user(self, user: UserCreate) -> UserModel:
         role = self.session.query(UserRole).where(UserRole.name == user.role).first()
@@ -29,8 +29,7 @@ class UserService:
             raise not_found("User already exists!")
 
         user_model = UserModel(**user.model_dump())
-        user_model.roleId = role.id
-        new_user = User(**user_model.model_dump())
+        new_user = User(**user_model.model_dump(), role=role)
 
         self.session.add(new_user)
         self.session.commit()
@@ -38,18 +37,23 @@ class UserService:
         return user_model
 
     def get_all_users(self) -> list[UserModel]:
-        user_query = self.session.query(User).options(joinedload(User.role)).all()
+        user_query: list[User] = (
+            self.session.query(User).options(joinedload(User.role)).all()
+        )
+
         users = []
-
         for user in user_query:
-            user_role = UserRoleModel.model_validate(user.role) if user.role else None
-
-            user_model = UserModel.model_validate({**user.__dict__, "role": user_role})
+            user_model = self.user_from_orm(user)
             users.append(user_model)
 
         return users
 
-    def user_exists(self, user: UserModel | UserCreate) -> bool:
+    def user_from_orm(self, user: User) -> UserModel:
+        user_role = UserRoleModel.model_validate(user.role) if user.role else None
+        user_model = UserModel.model_validate({**user.__dict__, "role": user_role})
+        return user_model
+
+    def user_exists(self, user: UserBase) -> bool:
         user_exists = self.session.query(
             exists().where(User.username == user.username, User.email == user.email)
         ).scalar()
