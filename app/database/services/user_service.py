918145d061785cdaa2863d438d02c1b354daf6eb
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-06 12:25:21 +0100

    Add get_all_users method to UserService

--- app/database/services/user_service.py
+++ app/database/services/user_service.py
@@ -1,15 +1,27 @@
 from sqlalchemy import exists
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 
 from app.functions.exceptions import not_found
 from app.functions.jwt import verify_password
 from app.models.user import User, UserRole
-from app.schemas.api.user import UserCreate, UserModel
+from app.schemas.api.user import UserCreate, UserModel, UserRoleModel
 
 
 class UserService:
     def __init__(self, session: Session) -> None:
         self.session = session
+
+    def get_all_users(self) -> list[UserModel]:
+        user_query = self.session.query(User).options(joinedload(User.role)).all()
+        users = []
+
+        for user in user_query:
+            user_role = UserRoleModel.model_validate(user.role) if user.role else None
+
+            user_model = UserModel.model_validate({**user.__dict__, "role": user_role})
+            users.append(user_model)
+
+        return users
 
     def authenticate_user(self, username: str, password: str) -> None | User:
         user = self.session.query(User).where(User.username == username).first()
@@ -20,7 +32,7 @@ class UserService:
         if verify_password(password, user.password):
             return user
 
-    def create_user(self, user: UserCreate) -> User:
+    def create_user(self, user: UserCreate) -> UserModel:
         role = self.session.query(UserRole).where(UserRole.name == user.role).first()
         if not role:
             raise not_found("Role does not exist!")
@@ -38,4 +50,4 @@ class UserService:
         self.session.add(new_user)
         self.session.commit()
 
-        return new_user
+        return user_model
