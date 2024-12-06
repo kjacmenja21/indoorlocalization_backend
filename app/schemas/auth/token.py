6520233fb27f303cd4a130c862750b77d01df4d9
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-07 00:55:20 +0100

    Refactor token class to support refresh tokens

--- app/schemas/auth/token.py
+++ app/schemas/auth/token.py
@@ -1,7 +1,7 @@
 from datetime import UTC, datetime, timedelta
 from typing import Optional
 
-from pydantic import BaseModel, Field, computed_field
+from pydantic import BaseModel, Field, ValidationError, computed_field
 
 from app.config import JWTConfig
 from app.functions.exceptions import forbidden, unprocessable_entity
@@ -20,7 +20,7 @@ class Token(BaseModel):
     scope: list[Role] = [Role.USER]
     expires_in: int
     data: TokenData
-    refresh_data: Optional[RefreshTokenData]
+    refresh_data: Optional[RefreshTokenData] = None
     iat: datetime = Field(default_factory=lambda: datetime.now(UTC))
 
     @computed_field
@@ -31,10 +31,14 @@ class Token(BaseModel):
         self.data.iat = self.iat
         self.data.exp = self.exp
 
-        access_token = create_token(self.data, JWTConfig().access_token_secret_key)
-        refresh_token = create_token(
-            self.refresh_data, JWTConfig().refresh_token_secret_key
-        )
+        access_token = self.generate_access_token()
+
+        refresh_token = None
+        if self.refresh_data:
+            refresh_token = create_token(
+                self.refresh_data,
+                JWTConfig().refresh_token_secret_key,
+            )
         return TokenEncode(
             access_token=access_token,
             refresh_token=refresh_token,
@@ -43,10 +47,28 @@ class Token(BaseModel):
             data=self.data,
         )
 
-    @classmethod
-    def decode(cls, token: str, scope: list[Role] | None = None) -> TokenDecode:
-        decoded_dict = decode_token(token, JWTConfig().access_token_secret_key)
+    def generate_access_token(self) -> str:
+        access_token = create_token(self.data, JWTConfig().access_token_secret_key)
+        return access_token
 
+    @classmethod
+    def decode_refresh(cls, token: str, key) -> RefreshTokenData | None:
+        decoded_dict = decode_token(token, key)
+        if not decoded_dict:
+            raise unprocessable_entity("Token invalid")
+
+        try:
+            refresh_token = RefreshTokenData.model_validate(decoded_dict)
+            return refresh_token
+        except ValidationError:
+            return None
+
+    @classmethod
+    def decode_access(
+        cls, token: str, key: str, scope: list[Role] | None = None
+    ) -> TokenDecode:
+        decoded_dict = decode_token(token, key)
+        print(decoded_dict)
         if not decoded_dict:
             raise unprocessable_entity("Token invalid")
 
