b3bcc951bcd776c2c851c16abb5c2ddcb6536b74
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-10 23:32:48 +0100

    Use args instead of file path to initialize config

--- app/functions/alembic_jobs.py
+++ app/functions/alembic_jobs.py
@@ -3,19 +3,18 @@ from typing import Any
 from alembic import command
 from alembic.config import Config
 from alembic.script import ScriptDirectory
-from sqlalchemy import CursorResult, Engine, inspect
+from sqlalchemy import CursorResult, Engine, inspect, text
 
 from app.database.settings import db_settings
 
 
 def create_config() -> Config:
-    new_config = Config
-    new_config.set_main_option(
-        new_config,
-        "sqlalchemy.url",
-        db_settings.db_dsn.render_as_string(hide_password=False),
-    )
-    return new_config
+    args = {
+        "sqlalchemy.url": db_settings.db_dsn.render_as_string(hide_password=False),
+        "script_location": "migrations",
+    }
+    alembic_cfg = Config(config_args=args)
+    return alembic_cfg
 
 
 def is_database_up_to_date(input_engine: Engine, alembic_cfg: Config) -> bool:
@@ -30,7 +29,7 @@ def is_database_up_to_date(input_engine: Engine, alembic_cfg: Config) -> bool:
 
     with input_engine.begin() as connection:
         cursor: CursorResult[Any] = connection.execute(
-            "SELECT version_num FROM alembic_version"
+            text("SELECT version_num FROM alembic_version")
         )
         result = cursor.fetchone()
         if not result:
@@ -45,6 +44,7 @@ def is_database_up_to_date(input_engine: Engine, alembic_cfg: Config) -> bool:
 
 
 def upgrade_database(alembic_cfg: Config) -> None:
+
     command.upgrade(alembic_cfg, "head")
 
 
