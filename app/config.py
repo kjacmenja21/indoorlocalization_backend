a0c8e5684768aa6bc63b2d761bc3bd8953a655e7
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-10 10:27:47 +0100

    Revert generate_config function, as it broke generate_env_example.py

--- app/config.py
+++ app/config.py
@@ -6,29 +6,33 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 ENV_FILE = ".env"
 
 
-def generate_config(env_prefix: str = "") -> SettingsConfigDict:
-    return SettingsConfigDict(
-        env_prefix=env_prefix,
+class GeneralConfig(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_prefix="",
         env_file=ENV_FILE,
         env_ignore_empty=True,
     )
-
-
-class GeneralConfig(BaseSettings):
-    model_config = generate_config()
     log_level: Literal["INFO", "WARNING", "ERROR"] = "INFO"
     refresh_token_cookie_name: str = "refresh-token"
 
 
 class mDNSConfig(BaseSettings):
-    model_config = generate_config("mdns_")
+    model_config = SettingsConfigDict(
+        env_prefix="mdns_",
+        env_file=ENV_FILE,
+        env_ignore_empty=True,
+    )
     enable: bool = False
     hostname: str = "mdns_dev"
     port: int = 8001
 
 
 class JWTConfig(BaseSettings):
-    model_config = generate_config("jwt_")
+    model_config = model_config = SettingsConfigDict(
+        env_prefix="jwt_",
+        env_file=ENV_FILE,
+        env_ignore_empty=True,
+    )
     access_token_secret_key: str = "supersecretkey"
     refresh_token_secret_key: str = "refreshtokensecret"
     algorithm: str = "HS256"
@@ -37,22 +41,23 @@ class JWTConfig(BaseSettings):
 
 
 class FastAPISettings(BaseSettings):
-    model_config = generate_config("app_")
+    model_config = SettingsConfigDict(
+        env_prefix="app_",
+        env_file=ENV_FILE,
+        env_ignore_empty=True,
+    )
     title: str = "Indoor Localization Backend"
     description: str = "# TODO write me down!"
     version: str = "0.0.0"
-    debug: bool = True
+    debug: bool = False
 
 
-class ASGIConfig(BaseSettings):
+class HypercornConfig(Config):
     app: str = "app:app"
     host: str = "127.0.0.1"
     port: int = 8000
 
-
-class HypercornConfig(Config):
     def __init__(self) -> None:
         super().__init__()
-        config = ASGIConfig()
-        self.bind = f"{config.host}:{config.port}"
+        self.bind = f"{self.host}:{self.port}"
         self.loglevel = "INFO"
