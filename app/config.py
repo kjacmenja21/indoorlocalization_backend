f5fadfc5e37433f0a09a13d6fcc79bb4d6fa7065
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-10 13:23:40 +0100

    Add StringCheck if it's not empty

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
-from typing import Annotated, Any, Literal
+from typing import Annotated, Literal
 
 from hypercorn import Config
-from pydantic import AfterValidator, field_validator
+from pydantic import AfterValidator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -13,7 +13,7 @@ def check_secret_set(value: str):
     return value
 
 
-SecretCheck = Annotated[str, AfterValidator(check_secret_set)]
+StringCheck = Annotated[str, AfterValidator(check_secret_set)]
 
 
 class GeneralConfig(BaseSettings):
@@ -21,32 +21,17 @@ class GeneralConfig(BaseSettings):
         env_prefix="", env_file="", env_ignore_unset=False
     )
     log_level: Literal["INFO", "WARNING", "ERROR"] = "INFO"
-    refresh_token_cookie_name: str = "refresh-token"
+    refresh_token_cookie_name: StringCheck = "refresh-token"
 
     mdns_enable: bool = False
     mdns_hostname: str = "mdns_dev"
     mdns_port: int = 8001
 
-    jwt_access_token_secret_key: SecretCheck
-    jwt_refresh_token_secret_key: SecretCheck
+    jwt_access_token_secret_key: StringCheck
+    jwt_refresh_token_secret_key: StringCheck
     jwt_algorithm: str = "HS256"
     jwt_access_token_expire_minutes: int = 30
     jwt_refresh_token_expire_minutes: int = 10080
-
-    @field_validator(mode="after")
-    def check_fields_set(self, value: Any, field: str) -> Any:
-        """
-        This validator checks if the value of a field is properly set (not None or empty, if applicable).
-        The validator is applied to all fields with the 'after' mode.
-        """
-        if value is None:
-            raise ValueError(f"{field} must not be None.")
-        if isinstance(value, str) and not value.strip():
-            raise ValueError(f"{field} must not be empty.")
-        if isinstance(value, (int, float)) and value == 0:
-            raise ValueError(f"{field} must not be zero.")
-
-        return value
 
 
 class FastAPISettings(BaseSettings):
