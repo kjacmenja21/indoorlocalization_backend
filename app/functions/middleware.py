f80bc0a8a27345ba9a46cba76773c27d4ab059ea
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-10 22:58:11 +0100

    Add Alembic auto-updating if database is out-of-date

--- app/functions/middleware.py
+++ app/functions/middleware.py
@@ -3,10 +3,12 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from app.config import GeneralConfig
+from app.database.db import engine
+from app.functions.alembic_jobs import prepare_database
 from app.functions.logger import setup_logger
 from app.functions.multicast_dns import MulticastDNS, init_mdns
 from app.models.common import init_orm
+from migrations.env import config
 
 
 @asynccontextmanager
@@ -15,6 +17,10 @@ async def lifespan(_: FastAPI):
     logging.info("Starting the application")
 
     multicast_dns: MulticastDNS | None = None
+
+    logging.info("Checking if database is up to date...")
+    prepare_database(engine, config)
+    logging.info("Database is up to date with Alembic HEAD!")
 
     init_orm()
 
