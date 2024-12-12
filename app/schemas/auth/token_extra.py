f454abdea5ca2250a808936cf37a4a96d5154c3f
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-12 14:33:06 +0100

    Fix typooooo

--- app/schemas/auth/token_extra.py
+++ app/schemas/auth/token_extra.py
@@ -24,7 +24,7 @@ class TokenData(BaseModel):
     username: str
     email: EmailStr
     first_name: str
-    first_name: str
+    last_name: str
     contact: str
     role: UserRoleModel
     iat: Optional[datetime] = None
