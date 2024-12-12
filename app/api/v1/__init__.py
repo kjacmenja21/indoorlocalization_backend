e2fd864514b8e873a7175e70c39de711059b8cbe
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-12 21:55:45 +0100

    Define floormaps routes

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from app.api.v1.auth import auth_router
+from app.api.v1.floormaps import floormap_router
 from app.api.v1.users import user_router
 
 v1_router = APIRouter(prefix="/v1")
 
 v1_router.include_router(auth_router)
 v1_router.include_router(user_router)
+v1_router.include_router(floormap_router)
---
+++ app/api/v1/floormaps.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter
+
+from app.api.dependencies import get_current_user_with_scope
+from app.schemas.api.user import UserBase
+from app.schemas.auth.user import Role
+
+floormap_router = APIRouter(prefix="/floor-maps", tags=["Floor Maps"])
+
+
+@floormap_router.get("/")
+def retrieve_floor_maps(
+    _: UserBase = get_current_user_with_scope([Role.USER]),
+):
+    pass
+
+
+@floormap_router.post("/")
+def create_new_floor_map(
+    _: UserBase = get_current_user_with_scope([Role.ADMIN]),
+):
+    pass
+
+
+@floormap_router.delete("/{id}")
+def delete_floor_map_by_id(
+    id: int,
+    _: UserBase = get_current_user_with_scope([Role.ADMIN]),
+):
+    pass
