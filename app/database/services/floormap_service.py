695b5b7878e57ab32def523232ceb6a361e92e03
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-13 02:25:14 +0100

    Add create_floormap method to FloormapService

--- app/database/services/floormap_service.py
+++ app/database/services/floormap_service.py
@@ -1,6 +1,29 @@
+from sqlalchemy import and_
 from sqlalchemy.orm import Session
+
+from app.models.floor_map import FloorMap
+from app.schemas.api.floormap import FloormapBase, FloormapCreate, FloormapModel
 
 
 class FloormapService:
     def __init__(self, session: Session) -> None:
         self.session = session
+
+    def create_floormap(self, floormap: FloormapCreate, image: bytes) -> FloormapModel:
+        new_floormap = FloorMap(**floormap.model_dump(), image=image)
+
+        self.session.add(new_floormap)
+        self.session.commit()
+
+        return self.get_floormap(floormap)
+
+    def get_floormap(self, floormap: FloormapBase | int) -> FloormapModel:
+        filter_query = None
+        if isinstance(floormap, FloormapBase):
+            filter_query = FloorMap.name == floormap.name
+        if isinstance(floormap, int):
+            filter_query = FloorMap.id == floormap
+
+        found_floormap = self.session.query(FloorMap).filter(filter_query).first()
+
+        return FloormapModel.model_validate(found_floormap)
