e70c1fc84183203d9418cbefe03455fa5e22e9ca
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-13 13:30:19 +0100

    Add asset_router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from app.api.v1.assets import asset_router
 from app.api.v1.auth import auth_router
 from app.api.v1.user import user_router
 
@@ -7,3 +8,4 @@ v1_router = APIRouter(prefix="/v1")
 
 v1_router.include_router(auth_router)
 v1_router.include_router(user_router)
+v1_router.include_router(asset_router)
