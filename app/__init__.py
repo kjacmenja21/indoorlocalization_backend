be6ed19bcca13175577ffd1354867f459f1d1f70
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-13 10:35:56 +0100

    Allow CORS all origins

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import add_api_routers
 from app.config import FastAPISettings
@@ -6,8 +7,17 @@ from app.functions.middleware import lifespan
 
 
 def create_server() -> FastAPI:
-
+    origins = ["*"]
     app_instance = FastAPI(lifespan=lifespan, **FastAPISettings().model_dump())
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     add_api_routers(app_instance)
     return app_instance
 
