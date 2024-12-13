80d95ed059ec57d19399708a8a6f9f7a02c4099e
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-13 03:04:08 +0100

    Add get_floor_map_by_id route

--- app/api/v1/floormaps.py
+++ app/api/v1/floormaps.py
@@ -37,9 +37,23 @@ def create_new_floor_map(
     )
 
 
+@floormap_router.get("/{floormap_id}")
+def get_floor_map_by_id(
+    floormap_id: int,
+    floormap_service: FloormapServiceDep,
+    _: UserBase = get_current_user_with_scope([Role.USER]),
+) -> FloormapModel:
+    floormap = floormap_service.get_floormap(floormap_id)
+    return floormap
+
+
 @floormap_router.delete("/{floormap_id}")
 def delete_floor_map_by_id(
     floormap_id: int,
+    floormap_service: FloormapServiceDep,
     _: UserBase = get_current_user_with_scope([Role.ADMIN]),
-):
-    pass
+) -> JSONResponse:
+    floormap_service.delete_floor_map_by_id(floormap_id)
+    return JSONResponse(
+        {"message": f"Successfully deleted floormap with id {floormap_id}"}
+    )
--- app/database/services/floormap_service.py
+++ app/database/services/floormap_service.py
@@ -1,5 +1,7 @@
+from sqlalchemy import exists
 from sqlalchemy.orm import Session
 
+from app.functions.exceptions import conflict
 from app.models.floor_map import FloorMap
 from app.schemas.api.floormap import FloormapBase, FloormapCreate, FloormapModel
 
@@ -9,12 +11,21 @@ class FloormapService:
         self.session = session
 
     def create_floormap(self, floormap: FloormapCreate, image: bytes) -> FloormapModel:
+        if self.floormap_exists(floormap):
+            raise conflict()
         new_floormap = FloorMap(**floormap.model_dump(), image=image)
 
         self.session.add(new_floormap)
         self.session.commit()
 
         return FloormapModel.model_validate(new_floormap)
+
+    def delete_floor_map_by_id(self, floormap_id: int) -> None:
+        floormap = (
+            self.session.query(FloorMap).where(FloorMap.id == floormap_id).first()
+        )
+        self.session.delete(floormap)
+        self.session.commit()
 
     def get_floormap(self, floormap: FloormapBase | int) -> FloormapModel:
         filter_query = None
@@ -36,3 +47,8 @@ class FloormapService:
             floormaps.append(user_model)
 
         return floormaps
+
+    def floormap_exists(self, floormap: FloormapBase) -> bool:
+        query = exists().where((FloorMap.name == floormap.name))
+        floormap_exists = self.session.query(query).scalar()
+        return bool(floormap_exists)
