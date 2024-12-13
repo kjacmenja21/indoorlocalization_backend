3eb8467d141be3efa719fcf9419d23a756a7a720
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-13 15:00:25 +0100

    Start implementation on retrieve_assets API route

--- app/api/v1/assets.py
+++ app/api/v1/assets.py
@@ -1,7 +1,19 @@
-from fastapi import APIRouter
+from typing import Optional
+
+from fastapi import APIRouter, Query
+from pydantic import PositiveInt
+
+from app.api.dependencies import AssetServiceDep
 
 asset_router = APIRouter(prefix="/assets", tags=["Asset"])
 
 
 @asset_router.get("/")
-def retrieve_assets(): ...
+def retrieve_assets(
+    active: Optional[bool],
+    asset_service: AssetServiceDep,
+    page: PositiveInt = Query(0, gt=-1),
+    limit: PositiveInt = Query(1, gt=0),
+):
+    assets = asset_service.get_all_assets(active, page, limit)
+
