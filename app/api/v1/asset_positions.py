e9c15623bcba2e8fb4f49cba64d56e3fbfdde401
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-20 14:44:56 +0100

    Define routes for asset position API

--- app/api/v1/asset_positions.py
+++ app/api/v1/asset_positions.py
@@ -1,13 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
+
+from app.api.dependencies import AssetPositionDep, get_current_user_with_scope
+from app.schemas.api.asset_position import AssetPositionQuery
+from app.schemas.api.user import UserBase
+from app.schemas.auth.role_types import Role
 
 asset_position_router = APIRouter(prefix="asset-position", tags=["Asset Position"])
 
 
 @asset_position_router.get("/")
-def retrieve_asset_position():
+def retrieve_asset_position(
+    asset_position_service: AssetPositionDep,
+    data: AssetPositionQuery = Query(),
+    _: UserBase = get_current_user_with_scope([Role.USER]),
+):
     pass
 
 
 @asset_position_router.post("/")
-def record_new_asset_position():
+def record_new_asset_position(
+    asset_position_service: AssetPositionDep,
+    _: UserBase = get_current_user_with_scope([Role.ADMIN]),
+):
     pass
