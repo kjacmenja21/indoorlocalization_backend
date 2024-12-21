7145a78939e8e8cffd5e82507058b71a3f9bc772
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-22 00:07:05 +0100

    Add on_message handler support

--- app/functions/mqtt_client.py
+++ app/functions/mqtt_client.py
@@ -1,3 +1,4 @@
+import inspect
 import logging
 from typing import Any
 
@@ -6,50 +7,66 @@ from fastapi_mqtt.config import MQTTConfig
 from fastapi_mqtt.fastmqtt import FastMQTT
 
 from app.config import GeneralConfig
+from app.schemas.mqtt.message import MQTTTopicHandler
 
 
 class MQTTClientHandler:
 
-    async def start(self):
+    async def start(self) -> None:
         await self.fast_mqtt.mqtt_startup()
 
-    async def stop(self):
+    async def stop(self) -> None:
         await self.fast_mqtt.mqtt_shutdown()
 
-    def __init__(self):
-        general_config = GeneralConfig()
+    def register_topic_handler(self, handler: MQTTTopicHandler) -> None:
+        self.fast_mqtt.client.subscribe(handler.topic)
+        self._topic_handlers.append(handler)
+
+    async def _handle_topic(self, topic: str, payload: bytes) -> None:
+        for handler_entry in self._topic_handlers:
+            if handler_entry.topic == topic:
+                handler = handler_entry.handler
+                if inspect.iscoroutinefunction(handler):
+                    # If the handler is awaitable
+                    await handler(topic, payload)
+                else:
+                    # If the handler is a normal function
+                    handler(topic, payload)
+                return
+
+    def __init__(self) -> None:
+        self.config = GeneralConfig()
         fast_mqtt = FastMQTT(
             config=MQTTConfig(
-                host=general_config.mqtt_host,
-                port=general_config.mqtt_port,
-                username=general_config.mqtt_username,
-                password=general_config.mqtt_password,
+                host=self.config.mqtt_host,
+                port=self.config.mqtt_port,
+                username=self.config.mqtt_username,
+                password=self.config.mqtt_password,
             )
         )
+        self.fast_mqtt = fast_mqtt
+        self.setup_decorators()
+        self._topic_handlers: list[MQTTTopicHandler] = []
 
-        @fast_mqtt.on_connect()
+    def setup_decorators(self):
+        mqtt = self.fast_mqtt
+
+        @mqtt.on_connect()
         def connect(_client: MQTTClient, _flags: int, _rc: int, _properties: Any):
-            fast_mqtt.client.subscribe("/test/topic")  # subscribing mqtt topic
+            mqtt.client.subscribe("/test/topic")  # subscribing mqtt topic
             logging.info(
                 "Connected to MQTT broker %s:%d",
-                general_config.mqtt_host,
-                general_config.mqtt_port,
+                self.config.mqtt_host,
+                self.config.mqtt_port,
             )
 
-        @fast_mqtt.on_message()
+        @mqtt.on_message()
         async def message(
-            _client: MQTTClient, topic: str, payload: bytes, qos: int, properties: Any
-        ):
-            logging.info(
-                "[MQTT Message] %s | %s | %s | %s",
-                topic,
-                payload.decode(),
-                qos,
-                properties,
-            )
-            return 0
+            _client: MQTTClient, topic: str, payload: bytes, _qos: int, _properties: Any
+        ) -> None:
+            return await self._handle_topic(topic, payload)
 
-        @fast_mqtt.subscribe("my/mqtt/topic/#")
+        @mqtt.subscribe("my/mqtt/topic/#")
         async def message_to_topic(
             _client: MQTTClient, topic: str, payload: bytes, qos: int, properties: Any
         ):
@@ -61,16 +78,14 @@ class MQTTClientHandler:
                 properties,
             )
 
-        @fast_mqtt.on_disconnect()
+        @mqtt.on_disconnect()
         def disconnect(_client: MQTTClient, _packet: Any, _exc: Any | None = None):
             logging.info(
                 "[MQTT] Disconnected from MQTT broker %s:%d",
-                general_config.mqtt_host,
-                general_config.mqtt_port,
+                self.config.mqtt_host,
+                self.config.mqtt_port,
             )
 
-        @fast_mqtt.on_subscribe()
-        def subscribe(client: MQTTClient, mid, qos: int, properties: Any):
-            print("subscribed", client, mid, qos, properties)
-
-        self.fast_mqtt = fast_mqtt
+        @mqtt.on_subscribe()
+        def subscribe(_client: MQTTClient, _mid, _qos: int, _properties: Any) -> None:
+            logging.info("[MQTT] Subscribed to topic")
--- app/schemas/mqtt/message.py
+++ app/schemas/mqtt/message.py
@@ -1,4 +1,20 @@
-from pydantic import BaseModel
+from typing import Callable
+
+from pydantic import BaseModel, field_validator
+
+
+class MQTTTopicHandler:
+    topic: str
+    handler: Callable[[str, bytes], None]
+
+    @classmethod
+    @field_validator("topic", mode="before")
+    def validate_topic(cls, topic: str) -> str:
+        if not topic or "#" in topic or "+" in topic:
+            raise ValueError(
+                "Invalid topic name. Wildcards are not allowed in registration."
+            )
+        return topic
 
 
 class MQTTAssetUpdateMessage(BaseModel):
