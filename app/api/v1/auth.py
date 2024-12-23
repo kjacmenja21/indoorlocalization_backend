9e15a281b19c190ecef0ce723bea4cd103cdb542
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-23 13:01:45 +0100

    Fix autologin logic to return same as login API route

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends, Response
-from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordRequestForm
 
 from app.api.dependencies import UserServiceDep, check_refresh_token_cookie
 from app.config import GeneralConfig
 from app.functions.exceptions import unauthorized, unauthorized_bearer
+from app.schemas.api.user import UserModelIndentified
 from app.schemas.auth.token import Token
 from app.schemas.auth.token_extra import RefreshTokenData, TokenData, TokenEncode
 
@@ -22,6 +22,36 @@ def login(
     if not user:
         raise unauthorized_bearer()
 
+    token = generate_token(response, user)
+
+    return token
+
+
+@auth_router.post("/autologin")
+async def refresh_token(
+    response: Response,
+    user_service: UserServiceDep,
+    refresh_tkn: str = Depends(check_refresh_token_cookie),
+) -> TokenEncode:
+    if not refresh_tkn:
+        raise unauthorized("No refresh token")
+
+    decoded_token = Token.decode_refresh(
+        refresh_tkn, GeneralConfig().jwt_refresh_token_secret_key
+    )
+    if not decoded_token:
+        raise unauthorized("Invalid refresh token")
+
+    user = user_service.get_user(user=decoded_token.client_id)
+    if not user:
+        raise unauthorized("User does not exist")
+
+    token = generate_token(response, user)
+
+    return token
+
+
+def generate_token(response: Response, user: UserModelIndentified):
     data = TokenData.model_validate(user)
     data.scope = [user.role.name]
     refresh_data = RefreshTokenData(client_id=user.id)
@@ -42,35 +72,3 @@ def login(
     )
 
     return token
-
-
-@auth_router.post("/autologin")
-async def refresh_token(
-    user_service: UserServiceDep,
-    refresh_tkn: str = Depends(check_refresh_token_cookie),
-) -> JSONResponse:
-    if not refresh_tkn:
-        raise unauthorized("No refresh token")
-
-    decoded_token = Token.decode_refresh(
-        refresh_tkn, GeneralConfig().jwt_refresh_token_secret_key
-    )
-    if not decoded_token:
-        raise unauthorized("Invalid refresh token")
-
-    user = user_service.get_user(user=decoded_token.client_id)
-    if not user:
-        raise unauthorized("User does not exist")
-
-    data = TokenData.model_validate(user)
-    data.scope = [user.role.name]
-
-    token = Token(
-        expires_in=GeneralConfig().jwt_access_token_expire_minutes,
-        data=data,
-        scope=data.scope,
-    )
-    access_token = token.generate_access_token()
-    return JSONResponse(
-        {"access_token": access_token, "email": user.email}, status_code=200
-    )
