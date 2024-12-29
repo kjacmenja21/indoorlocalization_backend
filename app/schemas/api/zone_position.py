4fd036274782d5fc2b1b6ccc6e4093cb5e965a25
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-29 14:19:56 +0100

    Use round_up_to_hour in AssetZonePositionQuery

--- app/schemas/api/zone_position.py
+++ app/schemas/api/zone_position.py
@@ -1,9 +1,11 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from pydantic import BaseModel
+
+from app.schemas.api.common import round_up_to_hour
 
 
 class AssetZonePositionQuery(BaseModel):
     assetId: int
-    startDate: datetime
-    endDate: datetime
+    startDate: datetime = round_up_to_hour(datetime.now() - timedelta(days=1))
+    endDate: datetime = round_up_to_hour(datetime.now())
