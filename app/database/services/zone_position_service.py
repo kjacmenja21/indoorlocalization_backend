aff9be6256a385f9be2eaeef44e78975b15ef1c5
kjacmenja21 <kjacmenja21@student.foi.hr>
2024-12-29 14:35:41 +0100

    Create ZonePositionService

---
+++ app/database/services/zone_position_service.py
@@ -0,0 +1,29 @@
+from sqlalchemy.orm import Session
+
+from app.models.history import AssetZoneHistory
+from app.schemas.api.zone_position import AssetZoneHistoryModel, AssetZonePositionQuery
+
+
+class ZonePositionService:
+    def __init__(self, session: Session) -> None:
+        self.session = session
+
+    def get_asset_zone_position_history(
+        self, query: AssetZonePositionQuery
+    ) -> list[AssetZoneHistoryModel]:
+
+        after_start_date = (AssetZoneHistory.enterDateTime >= query.startDate) | (
+            AssetZoneHistory.exitDateTime >= query.startDate
+        )
+
+        before_end_date = (AssetZoneHistory.enterDateTime <= query.endDate) | (
+            AssetZoneHistory.exitDateTime <= query.endDate
+        )
+
+        results = (
+            self.session.query(AssetZoneHistory)
+            .filter(after_start_date & before_end_date)
+            .all()
+        )
+
+        return [AssetZoneHistoryModel.model_validate(r) for r in results]
--- app/schemas/api/zone_position.py
+++ app/schemas/api/zone_position.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 from app.schemas.api.common import round_up_to_hour
 
@@ -9,3 +9,15 @@ class AssetZonePositionQuery(BaseModel):
     assetId: int
     startDate: datetime = round_up_to_hour(datetime.now() - timedelta(days=1))
     endDate: datetime = round_up_to_hour(datetime.now())
+
+
+class AssetZoneHistoryBase(BaseModel):
+    assetId: int
+    zoneId: int
+    enterDateTime: datetime
+    exitDateTime: datetime
+
+
+class AssetZoneHistoryModel(AssetZoneHistoryBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
