38e320bd04b3d7d761859d43f180d2f7aa628fc2
kjacmenja21 <kjacmenja21@student.foi.hr>
2025-01-10 16:03:56 +0100

    Start testing from scratch

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -3,32 +3,8 @@ from typing import Any, Generator
 import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.engine.base import Engine
-from sqlalchemy.orm import Session, sessionmaker
 
 from app import create_server
-from app.models.common import Base
-
-
-@pytest.fixture(scope="module")
-def mock_engine() -> Generator[Engine, Any, None]:
-    """Create an in-memory SQLite database for testing"""
-    engine = create_engine(
-        "sqlite:///:memory:", connect_args={"check_same_thread": False}
-    )
-    Base.metadata.create_all(bind=engine)
-    yield engine
-    Base.metadata.drop_all(bind=engine)
-
-
-@pytest.fixture
-def mock_session(mock_engine) -> Generator[Session, Any, None]:
-    """Create a session to interact with the mock in-memory database"""
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=mock_engine)
-    db = SessionLocal()
-    yield db
-    db.close()
 
 
 def mock_lifespan(_: FastAPI) -> Generator[None, Any, None]:
--- tests/unit/test_auth_api.py
+++ tests/unit/test_auth_api.py
@@ -1,24 +1,5 @@
 import pytest
 
-from app.schemas.api.user import UserModelIndentified
-
-
-# Mock the dependencies
-@pytest.fixture
-def mock_user_service():
-    class MockUserService:
-        def authenticate_user(self, username, password):
-            if username == "valid_user" and password == "valid_pass":
-                return UserModelIndentified(id=1, username="valid_user", role="user")
-            return None
-
-        def get_user(self, user):
-            if user == 1:
-                return UserModelIndentified(id=1, username="valid_user", role="user")
-            return None
-
-    return MockUserService()
-
 
 @pytest.fixture
 def mock_check_refresh_token_cookie():
@@ -42,7 +23,7 @@ def create_url(url: str):
     return "/api/v1/" + url
 
 
-def test_login_valid(client, mock_user_service):
+def test_login_valid(client):
     # Mock form data
     url = create_url("auth/login")
     form_data = {"username": "valid_user", "password": "valid_pass"}
@@ -52,31 +33,9 @@ def test_login_valid(client, mock_user_service):
     assert "refresh_token" in response.cookies
 
 
-def test_login_invalid(client, mock_user_service):
+def test_login_invalid(client):
     # Mock form data
     url = create_url("auth/login")
     form_data = {"username": "invalid_user", "password": "wrong_pass"}
     response = client.post(url, data=form_data)
-    assert response.status_code == 401  # Unauthorized
-
-
-def test_autologin_valid(
-    client, mock_user_service, mock_check_refresh_token_cookie, mock_general_config
-):
-    # Mock valid refresh token
-    url = create_url("auth/autologin")
-    refresh_token = "valid_refresh_token"
-    response = client.post(url, cookies={"refresh_token": refresh_token})
-    assert response.status_code == 200
-    assert "access_token" in response.json()
-    assert "refresh_token" in response.cookies
-
-
-def test_autologin_invalid_token(
-    client, mock_user_service, mock_check_refresh_token_cookie
-):
-    # Test invalid refresh token
-    url = create_url("auth/autologin")
-    refresh_token = "invalid_refresh_token"
-    response = client.post(url, cookies={"refresh_token": refresh_token})
     assert response.status_code == 401  # Unauthorized
