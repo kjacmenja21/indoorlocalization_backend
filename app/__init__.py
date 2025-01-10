2245f076790d0d0705680aa26ddd3d0deab15269
kjacmenja21 <kjacmenja21@student.foi.hr>
2025-01-10 10:31:37 +0100

    Refactor create_server to take a lifespan to use dependency injection prinicple

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,16 @@
+from typing import Callable
+
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import add_api_routers
 from app.config import FastAPISettings
 from app.functions.middleware import lifespan
 
 
-def create_server() -> FastAPI:
-    origins = ["*"]
-    app_instance = FastAPI(lifespan=lifespan, **FastAPISettings().model_dump())
-
-    print("\nadd_middleware\nTODO: Update origins to not be '*' (other allow_*'s too)")
-    app_instance.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
+def create_server(lifespan_function: Callable[[FastAPI], None]) -> FastAPI:
+    app_instance = FastAPI(lifespan=lifespan_function, **FastAPISettings().model_dump())
     add_api_routers(app_instance)
     return app_instance
 
 
-app = create_server()
+app = create_server(lifespan)
