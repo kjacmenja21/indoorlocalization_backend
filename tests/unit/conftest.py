fd1fe287d45d8de531046afc39ca80ead5a3dea9
kjacmenja21 <kjacmenja21@student.foi.hr>
2025-01-11 16:01:32 +0100

    Move creates to util

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -5,75 +5,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 from app import MiddlewareItem, create_server
 from app.models.common import Base
-from app.models.user import UserRole
-from app.schemas.auth.role_types import Role
-from tests.unit.util import get_floormap
-
-
-def create_user_roles(session: Session):
-    # Define the function to create user roles
-    admin_role = UserRole(name=Role.ADMIN)
-    user_role = UserRole(name=Role.USER)
-    session.add_all([admin_role, user_role])
-    session.commit()
-
-
-def create_users(session: Session):
-    from app.models.user import User
-    from app.schemas.api.user import UserCreate
-
-    users_data = [
-        UserCreate(
-            username="admin",
-            email="admin@example.com",
-            plain_password="adminpass",
-            role=Role.ADMIN,
-        ),
-        UserCreate(
-            username="user",
-            email="user@example.com",
-            plain_password="userpass",
-            role=Role.USER,
-        ),
-        UserCreate(
-            username="testuser",
-            email="testuser@example.com",
-            plain_password="testpass",
-            role=Role.USER,
-        ),
-        UserCreate(
-            username="delete_user",
-            email="delete_user@example.com",
-            plain_password="deletepass",
-            role=Role.USER,
-        ),
-    ]
-
-    roles = session.query(UserRole).all()
-    commit_users = []
-    for user in users_data:
-        commit_user = User(
-            **user.model_dump(exclude_none=True, exclude=["plain_password", "role"])
-        )
-        commit_user.role = next(role for role in roles if role.name == user.role)
-        commit_users.append(commit_user)
-
-    session.add_all(commit_users)
-    session.commit()
-
-
-def create_floormaps(session: Session):
-    floormaps = [
-        get_floormap("First Floor 1"),
-        get_floormap("Second Floor 1"),
-        get_floormap("Third Floor 1"),
-    ]
-    session.add_all(floormaps)
-    session.commit()
 
 
 def mock_lifespan(_: FastAPI) -> Generator[None, Any, None]:
@@ -112,9 +47,6 @@ def mock_session():
     SessionMaker = sessionmaker(bind=engine)
     session = SessionMaker()
 
-    create_user_roles(session)
-    create_users(session)
-    create_floormaps(session)
     yield session
 
     # Teardown mock database connection
--- tests/unit/util.py
+++ tests/unit/util.py
@@ -1,8 +1,15 @@
 import uuid
 from functools import lru_cache
+from random import randint
+from typing import Any
+
+from sqlalchemy.orm import Session
 
 from app.models.floor_map import FloorMap
+from app.models.user import UserRole
 from app.schemas.api.asset import AssetCreate
+from app.schemas.api.zone import ZoneCreate, ZonePointBase
+from app.schemas.auth.role_types import Role
 
 
 @lru_cache(maxsize=32)
@@ -12,9 +19,71 @@ def get_test_image() -> bytes:
     return image_data
 
 
+def create_user_roles(session: Session):
+    # Define the function to create user roles
+    admin_role = UserRole(name=Role.ADMIN)
+    user_role = UserRole(name=Role.USER)
+    session.add_all([admin_role, user_role])
+    session.commit()
+
+
+def create_users(session: Session):
+    from app.models.user import User
+    from app.schemas.api.user import UserCreate
+
+    users_data = [
+        UserCreate(
+            username="admin",
+            email="admin@example.com",
+            plain_password="adminpass",
+            role=Role.ADMIN,
+        ),
+        UserCreate(
+            username="user",
+            email="user@example.com",
+            plain_password="userpass",
+            role=Role.USER,
+        ),
+        UserCreate(
+            username="testuser",
+            email="testuser@example.com",
+            plain_password="testpass",
+            role=Role.USER,
+        ),
+        UserCreate(
+            username="delete_user",
+            email="delete_user@example.com",
+            plain_password="deletepass",
+            role=Role.USER,
+        ),
+    ]
+
+    roles = session.query(UserRole).all()
+    commit_users = []
+    for user in users_data:
+        commit_user = User(
+            **user.model_dump(exclude_none=True, exclude=["plain_password", "role"])
+        )
+        commit_user.role = next(role for role in roles if role.name == user.role)
+        commit_users.append(commit_user)
+
+    session.add_all(commit_users)
+    session.commit()
+
+
+def create_floormaps(session: Session):
+    floormaps = [
+        get_floormap("First Floor 1"),
+        get_floormap("Second Floor 1"),
+        get_floormap("Third Floor 1"),
+    ]
+    session.add_all(floormaps)
+    session.commit()
+
+
 def get_floormap(name: str):
     return FloorMap(
-        name=name,
+        name=name + uuid.uuid4().hex,
         image=get_test_image(),
         image_type="png",
         width=100.0,
@@ -30,3 +99,28 @@ def get_asset(floormap: FloorMap) -> AssetCreate:
     return AssetCreate(
         name="Test Asset" + uuid.uuid4().hex, active=True, floormap_id=floormap.id
     )
+
+
+def get_zone(floormap: FloorMap) -> tuple[int, ZoneCreate]:
+    points = [
+        ZonePointBase(
+            ordinalNumber=0,
+            x=randint(0, 50),
+            y=randint(0, 50),
+        ),
+        ZonePointBase(
+            ordinalNumber=1,
+            x=randint(51, 100),
+            y=randint(0, 50),
+        ),
+        ZonePointBase(
+            ordinalNumber=2,
+            x=randint(0, 100),
+            y=randint(51, 100),
+        ),
+    ]
+    color = int("0x0000FF", 16)
+    zone_create = ZoneCreate(
+        name="New Zone", floorMapId=floormap.id, color=color, points=points
+    )
+    return color, zone_create
