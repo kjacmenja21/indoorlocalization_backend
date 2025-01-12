5ec9a05942d3c2b0aade42999835f380df40a1ef
kjacmenja21 <kjacmenja21@student.foi.hr>
2025-01-12 08:50:07 +0100

    Refactor environment file handling to support multiple configurations and streamline the process

--- cicd/prepare_env.py
+++ cicd/prepare_env.py
@@ -9,19 +9,42 @@ required_env_vars = {
     "JWT_REFRESH_TOKEN_SECRET_KEY": "default_refresh_secret",
 }
 
-# Read the .example.env file
-with open(".example.env", "r", encoding="utf-8") as example_file:
-    lines = example_file.readlines()
+required_env_app_vars = {
+    "APP_ADMIN": "default_admin",
+    "APP_PASSWORD": "default_password",
+}
 
-# Create or overwrite the .env file
-with open(".env", "w", encoding="utf-8") as env_file:
-    for line in lines:
-        # Split the line into key and value
-        if "=" in line:
-            key, value = line.strip().split("=", 1)
-            # If the key is in the required_env_vars, use the default value if the value is empty
-            if key in required_env_vars and not value:
-                value = required_env_vars[key]
-            env_file.write(f"{key}={value}\n")
-        else:
-            env_file.write(line)
+required_env_db_vars = {
+    "DB_POSTGRES_URL": "postgresql://postgres:example@localhost:5432/postgres",
+    "DB_USER": "dev",
+    "DB_PASSWORD": "dev_password",
+}
+
+
+def fill_env_file(example_file_path, env_file_path, required_vars):
+    # Read the example env file
+    with open(example_file_path, "r", encoding="utf-8") as example_file:
+        lines = example_file.readlines()
+
+    # Create or overwrite the env file
+    with open(env_file_path, "w", encoding="utf-8") as env_file:
+        for line in lines:
+            # Split the line into key and value
+            if "=" in line:
+                key, value = line.strip().split("=", 1)
+                # If the key is in the required_vars, use the default value if the value is empty
+                if key in required_vars and not value:
+                    value = required_vars[key]
+                env_file.write(f"{key}={value}\n")
+            else:
+                env_file.write(line)
+
+
+# Fill out .env
+fill_env_file(".example.env", ".env", required_env_vars)
+
+# Fill out .env.app
+fill_env_file(".example.env.app", ".env.app", required_env_app_vars)
+
+# Fill out .env.db
+fill_env_file(".env.db.example", ".env.db", required_env_db_vars)
